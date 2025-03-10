a429e97261a4086b661e811f6c680a73b53d79c9
Rory James <rory.james2021@gmail.com>
2025-03-09 17:36:52 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int res = 0;
+		while (x < 10) {
+			res = res + x;
+			System.out.print(res + " ");
+			x = x + 1;
+		}
+		System.out.println();
+	}
+}
