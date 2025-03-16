72a62871ac94de200afcb3855c94127a5fce07d4
Rory James <rory.james2021@gmail.com>
2025-03-15 18:41:58 -0700

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,17 @@ public class LeapYear {
             }
         }
     }
+    
+    /** Determines whether a year is a leap year. */
+    public static boolean isLeapYear(int year) {
+	if (year % 400 == 0) {
+	    return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }	
+
 }
 
